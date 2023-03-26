f80a2f05c0959eb2e66755f73a57314b21636348
ThanhCong0404 <Thanhcong04042001@gmail.com>
2023-03-26 10:27:11 +0700

    design home page and init routing url for system

--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php
 
+define("ROOT", "http://localhost/BlogProject/public");
 
-echo "Home Page";
+
+// ROUTING PAGE
+$url = $_GET['url'] ?? 'home'; //default home if dont exist param url 
+$url = explode("/", $url); //split
+
+$page_name = trim($url[0]);
+$filename = "../app/pages/".$page_name.".php";
+
+if(file_exists($filename)){
+	require_once $filename;
+}else{
+	require_once  "../app/pages/404.php";
+}
+
 ?>
