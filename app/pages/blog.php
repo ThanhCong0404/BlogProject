d540a5c1ddde292a032d78508b5134bb6e3889f9
Nguyễn Thành Công <92791167+ThanhCong0404@users.noreply.github.com>
2024-05-16 08:58:15 +0700

    improve

--- app/pages/blog.php
+++ app/pages/blog.php
@@ -1,44 +1,46 @@
-
 <?php include '../app/pages/includes/header.php'; ?>
 
 <div class="mx-auto col-md-10">
-    <h3 class="mx-4">Blog</h3>
+  <h3 class="mx-4">Blog</h3>
 
-      <div class="row my-2 justify-content-center">
+  <div class="row my-2 justify-content-center">
+    <?php
+    $limit = 10;
+    $offset = ($PAGE['page_number'] - 1) * $limit;
 
-        <?php  
+    $query = "select posts.*,categories.category from posts join categories on posts.category_id = categories.id order by id desc limit $limit offset $offset";
+    $rows = query($query);
 
-          $limit = 10;
-          $offset = ($PAGE['page_number'] - 1) * $limit;
-
-          $query = "select posts.*,categories.category from posts join categories on posts.category_id = categories.id order by id desc limit $limit offset $offset";
-          $rows = query($query);
-          if($rows)
-          {
-            foreach ($rows as $row) {
-              include '../app/pages/includes/post-card.php';
-            }
-
-          }else{
-            echo "No items found!";
-          }
-
-        ?>
-
-      </div>
-
+    if ($rows) {
+      foreach ($rows as $row) {
+        include '../app/pages/includes/post-card.php';
+      }
+    } else {
+      echo "No items found!";
+    }
+    ?>
+  </div>
 
   <div class="col-md-12 mb-4">
-    <a href="<?=$PAGE['first_link']?>">
-      <button class="btn btn-primary">First Page</button>
-    </a>
-    <a href="<?=$PAGE['prev_link']?>">
-      <button class="btn btn-primary">Prev Page</button>
-    </a>
-    <a href="<?=$PAGE['next_link']?>">
-      <button class="btn btn-primary float-end">Next Page</button>
-    </a>
+    <?php
+    $total_pages = ceil(count(query("select * from posts")) / $limit);
+
+    if ($PAGE['page_number'] > 1) {
+      echo '<a href="' . $PAGE['first_link'] . '">';
+      echo '<button class="btn btn-primary">First Page</button>';
+      echo '</a>';
+
+      echo '<a href="' . $PAGE['prev_link'] . '">';
+      echo '<button class="btn btn-primary">Prev Page</button>';
+      echo '</a>';
+    }
+
+    if ($PAGE['page_number'] < $total_pages) {
+      echo '<a href="' . $PAGE['next_link'] . '">';
+      echo '<button class="btn btn-primary float-end">Next Page</button>';
+      echo '</a>';
+    }
+    ?>
   </div>
 </div>
 <?php include '../app/pages/includes/footer.php'; ?>
-
