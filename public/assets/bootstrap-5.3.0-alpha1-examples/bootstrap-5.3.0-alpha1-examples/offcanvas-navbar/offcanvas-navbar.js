e0116832e1f493ccf845df01dbc8c9b64e57b997
Nguyễn Thành Công <92791167+ThanhCong0404@users.noreply.github.com>
2024-05-16 08:59:35 +0700

    improve

--- public/assets/bootstrap-5.3.0-alpha1-examples/bootstrap-5.3.0-alpha1-examples/offcanvas-navbar/offcanvas-navbar.js
+++ public/assets/bootstrap-5.3.0-alpha1-examples/bootstrap-5.3.0-alpha1-examples/offcanvas-navbar/offcanvas-navbar.js
@@ -1,7 +1,20 @@
 (() => {
   'use strict'
 
-  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  document.addEventListener('click', (event) => {
+    if (event.target.id === 'navbarSideCollapse') {
+      document.querySelector('.offcanvas-collapse').classList.toggle('open')
+    }
+  })
+})()
+
+
+(() => {
+  'use strict'
+
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('#navbarSideCollapse')) {
+      document.querySelector('.offcanvas-collapse').classList.toggle('open')
+    }
   })
 })()
