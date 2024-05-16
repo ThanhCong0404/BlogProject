45e0c8992fcc29101d6c13920ae33928a55fcead
Nguyễn Thành Công <92791167+ThanhCong0404@users.noreply.github.com>
2024-05-16 08:58:11 +0700

    improve

--- app/pages/home.php
+++ app/pages/home.php
@@ -1,26 +1,30 @@
+<?php
+include '../app/pages/includes/header.php';
 
-<?php include '../app/pages/includes/header.php'; ?>
+$query = "select posts.*,categories.category from posts join categories on posts.category_id = categories.id order by id desc limit 6";
+$rows = query($query);
+
+if ($rows === false) {
+    echo "Error executing query";
+} else {
+    if (empty($rows)) {
+        echo "No posts found!";
+    } else {
+        foreach ($rows as $row) {
+            if (file_exists('../app/pages/includes/post-card.php')) {
+                include '../app/pages/includes/post-card.php';
+            } else {
+                echo "post-card.php not found!";
+            }
+        }
+    }
+}
+?>
 
 <h3 class="mx-4">Featured</h3>
 
-  <div class="row my-2 justify-content-center">
+<div class="row my-2 justify-content-center">
 
-    <?php  
+</div>
 
-      $query = "select posts.*,categories.category from posts join categories on posts.category_id = categories.id order by id desc limit 6";
-      $rows = query($query);
-      if($rows)
-      {
-        foreach ($rows as $row) {
-          include '../app/pages/includes/post-card.php';
-        }
-
-      }else{
-        echo "No items found!";
-      }
-
-    ?>
-
-  </div>
-
-  <?php include '../app/pages/includes/footer.php'; ?>
+<?php include '../app/pages/includes/footer.php'; ?>
