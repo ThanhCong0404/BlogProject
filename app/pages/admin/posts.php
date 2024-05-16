02b4fc2bb550be3924c400daaea3b6bb58fe21c5
Nguyễn Thành Công <92791167+ThanhCong0404@users.noreply.github.com>
2024-05-16 08:58:24 +0700

    improve

--- app/pages/admin/posts.php
+++ app/pages/admin/posts.php
@@ -1,285 +1,77 @@
-<?php if($action == 'add'):?>
+<?php
 
-<link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/summernote/summernote-lite.min.css">
+namespace App\Controllers;
 
-<div class="col-md-12 mx-auto">
-  <form method="post" enctype="multipart/form-data">
+class PostsController
+{
+    public function index()
+    {
+        // Add your code here
+    }
 
-    <h1 class="h3 mb-3 fw-normal">Create post</h1>
+    public function add()
+    {
+        // Add your code here
+    }
 
-    <?php if (!empty($errors)):?>
-      <div class="alert alert-danger">Please fix the errors below</div>
-    <?php endif;?>
+    public function edit($id)
+    {
+        // Add your code here
+    }
 
-    <div class="my-2">
-      Featured Image:<br>
-      <label class="d-block">
-        <img class="mx-auto d-block image-preview-edit" src="<?=get_image('')?>" style="cursor: pointer;width: 150px;height: 150px;object-fit: cover;">
-        <input onchange="display_image_edit(this.files[0])" type="file" name="image" class="d-none">
-      </label>
-      <?php if(!empty($errors['image'])):?>
-        <div class="text-danger"><?=$errors['image']?></div>
-      <?php endif;?>
+    public function delete($id)
+    {
+        // Add your code here
+    }
+}
 
-      <script>
-        
-        function display_image_edit(file)
-        {
-          document.querySelector(".image-preview-edit").src = URL.createObjectURL(file);
-        }
-      </script>
-    </div>
+?>
 
-
-    <div class="form-floating">
-      <input value="<?=old_value('title')?>" name="title" type="text" class="form-control mb-2" id="floatingInput" placeholder="Username">
-      <label for="floatingInput">Title</label>
-    </div>
-      <?php if(!empty($errors['title'])):?>
-      <div class="text-danger"><?=$errors['title']?></div>
-      <?php endif;?>
-
-    <div class="">
-      <textarea id="summernote" rows="8" name="content" id="floatingInput" placeholder="Post content" type="content" class="form-control"><?=old_value('content')?></textarea>
-    </div>
-      <?php if(!empty($errors['content'])):?>
-      <div class="text-danger"><?=$errors['content']?></div>
-      <?php endif;?>
-
-    <div class="form-floating my-3">
-      <select name="category_id" class="form-select">
-
-        <?php  
-
-          $query = "select * from categories order by id desc";
-      $categories = query($query);
-        ?>
-        <option value="">--Select--</option>
-        <?php if(!empty($categories)):?>
-          <?php foreach($categories as $cat):?>
-            <option <?=old_select('category_id',$cat['id'])?> value="<?=$cat['id']?>"><?=$cat['category']?></option>
-          <?php endforeach;?>
-        <?php endif;?>
-
-      </select>
-      <label for="floatingInput">Category</label>
-    </div>
-      <?php if(!empty($errors['category'])):?>
-      <div class="text-danger"><?=$errors['category']?></div>
-      <?php endif;?>
-
-    <a href="<?=ROOT?>/admin/posts">
-      <button class="mt-4 btn btn-lg btn-primary" type="button">Back</button>
-  </a>
-    <button class="mt-4 btn btn-lg btn-primary float-end" type="submit">Create</button>
-   
-  </form>
-</div>
-
-<script src="<?=ROOT?>/assets/js/jquery.js"></script>
-<script src="<?=ROOT?>/assets/summernote/summernote-lite.min.js"></script>
-<script>
-    $('#summernote').summernote({
-      placeholder: 'Post content',
-      tabsize: 2,
-      height: 400
-    });
-  </script>
-
-<?php elseif($action == 'edit'):?>
-
-<link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/summernote/summernote-lite.min.css">
-
-<div class="col-md-12 mx-auto">
-  <form method="post" enctype="multipart/form-data">
-
-    <h1 class="h3 mb-3 fw-normal">Edit post</h1>
-
-    <?php if(!empty($row)):?>
-
-      <?php if (!empty($errors)):?>
-        <div class="alert alert-danger">Please fix the errors below</div>
-      <?php endif;?>
-
-      <div class="my-2">
-        <label class="d-block">
-          <img class="mx-auto d-block image-preview-edit" src="<?=get_image($row['image'])?>" style="cursor: pointer;width: 150px;height: 150px;object-fit: cover;">
-          <input onchange="display_image_edit(this.files[0])" type="file" name="image" class="d-none">
-        </label>
-        <?php if(!empty($errors['image'])):?>
-          <div class="text-danger"><?=$errors['image']?></div>
-        <?php endif;?>
-
-        <script>
-          
-          function display_image_edit(file)
-          {
-            document.querySelector(".image-preview-edit").src = URL.createObjectURL(file);
-          }
-        </script>
-      </div>
-
-      <div class="form-floating">
-        <input value="<?=old_value('title', $row['title'])?>" name="title" type="text" class="form-control mb-2" id="floatingInput" placeholder="Username">
-        <label for="floatingInput">Username</label>
-      </div>
-        <?php if(!empty($errors['title'])):?>
-        <div class="text-danger"><?=$errors['title']?></div>
-        <?php endif;?>
-
-      <div class="">
-      <textarea id="summernote" rows="8" name="content" id="floatingInput" placeholder="Post content" type="content" class="form-control"><?=old_value('content',add_root_to_images($row['content']))?></textarea>
-    </div>
-      <?php if(!empty($errors['content'])):?>
-      <div class="text-danger"><?=$errors['content']?></div>
-      <?php endif;?>
-
-    <div class="form-floating my-3">
-      <select name="category_id" class="form-select">
-
-        <?php  
-
-          $query = "select * from categories order by id desc";
-      $categories = query($query);
-        ?>
-        <option value="">--Select--</option>
-        <?php if(!empty($categories)):?>
-          <?php foreach($categories as $cat):?>
-            <option <?=old_select('category_id',$cat['id'],$row['category_id'])?> value="<?=$cat['id']?>"><?=$cat['category']?></option>
-          <?php endforeach;?>
-        <?php endif;?>
-
-      </select>
-      <label for="floatingInput">Category</label>
-    </div>
-      <?php if(!empty($errors['category'])):?>
-      <div class="text-danger"><?=$errors['category']?></div>
-      <?php endif;?>
-
-      <a href="<?=ROOT?>/admin/posts">
-        <button class="mt-4 btn btn-lg btn-primary" type="button">Back</button>
-    </a>
-      <button class="mt-4 btn btn-lg btn-primary  float-end" type="submit">Save</button>
-  <?php else:?>
-
-    <div class="alert alert-danger text-center">Record not found!</div>
-  <?php endif;?>
-
-  </form>
-</div>
-
-<script src="<?=ROOT?>/assets/js/jquery.js"></script>
-<script src="<?=ROOT?>/assets/summernote/summernote-lite.min.js"></script>
-<script>
-    $('#summernote').summernote({
-      placeholder: 'Post content',
-      tabsize: 2,
-      height: 400
-    });
-  </script>
-
-<?php elseif($action == 'delete'):?>
-
-<div class="col-md-6 mx-auto">
-  <form method="post">
-
-    <h1 class="h3 mb-3 fw-normal">Delete post</h1>
-
-    <?php if(!empty($row)):?>
-
-      <?php if (!empty($errors)):?>
-        <div class="alert alert-danger">Please fix the errors below</div>
-      <?php endif;?>
-
-      <div class="form-floating">
-        <div class="form-control mb-2" ><?=old_value('title', $row['title'])?></div>
-      </div>
-        <?php if(!empty($errors['title'])):?>
-        <div class="text-danger"><?=$errors['title']?></div>
-        <?php endif;?>
-
-      <div class="form-floating">
-        <div class="form-control mb-2" ><?=old_value('slug', $row['slug'])?></div>
-      </div>
-        <?php if(!empty($errors['slug'])):?>
-        <div class="text-danger"><?=$errors['slug']?></div>
-        <?php endif;?>
-
-
-      <a href="<?=ROOT?>/admin/posts">
-        <button class="mt-4 btn btn-lg btn-primary" type="button">Back</button>
-    </a>
-      <button class="mt-4 btn btn-lg btn-danger  float-end" type="submit">Delete</button>
-  <?php else:?>
-
-    <div class="alert alert-danger text-center">Record not found!</div>
-  <?php endif;?>
-
-  </form>
-</div>
-
-<?php else:?>
-
-<h4>
-  Posts
-  <a href="<?=ROOT?>/admin/posts/add">
-    <button class="btn btn-primary">Add New</button>
-  </a>
-</h4>
-
-<div class="table-responsive">
-<table class="table">
-  
-  <tr>
-    <th>#</th>
-    <th>Title</th>
-    <th>Slug</th>
-    <th>Image</th>
-    <th>Date</th>
-    <th>Action</th>
-  </tr>
-  <?php  
-    $limit = 10;
-    $offset = ($PAGE['page_number'] - 1) * $limit;
-
-    $query = "select * from posts order by id desc limit $limit offset $offset";
-    $rows = query($query);
-  ?>
-
-  <?php if(!empty($rows)):?>
-    <?php foreach($rows as $row):?>
-    <tr>
-      <td><?=$row['id']?></td>
-      <td><?=esc($row['title'])?></td>
-      <td><?=$row['slug']?></td>
-      <td>
-        <img src="<?=get_image($row['image'])?>" style="width: 100px;height: 100px;object-fit: cover;">
-      </td>
-      <td><?=date("jS M, Y",strtotime($row['date']))?></td>
-      <td>
-        <a href="<?=ROOT?>/admin/posts/edit/<?=$row['id']?>">
-          <button class="btn btn-warning text-white btn-sm"><i class="bi bi-pencil-square"></i></button>
-        </a>
-        <a href="<?=ROOT?>/admin/posts/delete/<?=$row['id']?>">
-          <button class="btn btn-danger btn-sm"><i class="bi bi-trash-fill"></i></button>
-        </a>
-      </td>
-    </tr>
-    <?php endforeach;?>
-  <?php endif;?>
-</table>
-
-<div class="col-md-12 mb-4">
-  <a href="<?=$PAGE['first_link']?>">
-    <button class="btn btn-primary">First Page</button>
-  </a>
-  <a href="<?=$PAGE['prev_link']?>">
-    <button class="btn btn-primary">Prev Page</button>
-  </a>
-  <a href="<?=$PAGE['next_link']?>">
-    <button class="btn btn-primary float-end">Next Page</button>
-  </a>
-</div>
-</div>
-
-<?php endif;?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?=ROOT?>/assets/summernote/summernote-lite.min.css">
+</head>
+<body>
+    <div class="container">
+        <?php if ($action == 'add' || $action == 'edit'): ?>
+            <h1 class="my-4 text-center">
+                <?= $action == 'add' ? 'Add New' : 'Edit' ?> Post
+            </h1>
+            <?php if (!empty($errors)): ?>
+                <div class="alert alert-danger">Please fix the errors below</div>
+            <?php endif; ?>
+            <form action="<?= $action == 'add' ? ROOT . '/admin/posts/add' : ROOT . '/admin/posts/edit/' . $row['id'] ?>" method="post" enctype="multipart/form-data" novalidate>
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title" class="form-control <?= !empty($errors['title']) ? 'is-invalid' : '' ?>" value="<?= old_value('title', $row['title'] ?? '') ?>" required autofocus minlength="3" maxlength="255">
+                    <?php if (!empty($errors['title'])): ?>
+                        <div class="invalid-feedback"><?= $errors['title'] ?></div>
+                    <?php endif; ?>
+                </div>
+                <div class="form-group">
+                    <label for="image">Featured Image</label>
+                    <div class="custom-file">
+                        <input type="file" name="image" id="image" class="custom-file-input <?= !empty($errors['image']) ? 'is-invalid' : '' ?>" onchange="display_image_edit(this.files[0])" required>
+                        <label for="image" class="custom-file-label">Choose file</label>
+                        <?php if (!empty($errors['image'])): ?>
+                            <div class="invalid-feedback"><?= $errors['image'] ?></div>
+                        <?php endif; ?>
+                    </div>
+                    <img class="img-fluid mt-2 image-preview-edit" src="<?= get_image($row['image'] ?? '') ?>" alt="Featured Image">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea id="summernote" name="content" id="floatingInput" placeholder="Post content" class="form-control <?= !empty($errors['content']) ? 'is-invalid' : '' ?>" required><?= old_value('content', $row['content'] ?? '') ?></textarea>
+                    <?php if (!empty($errors['content'])): ?>
+                        <div class="invalid-feedback"><?= $errors['content'] ?></div>
+                    <?php endif; ?>
+                </div>
+                <div class="form-group">
+                    <label for="category_id">Category</label>
+                    <select name="category_id" id="category_id" class="form-control <?= !empty($errors['category']) ? 'is-invalid' : '' ?>" required>
+                        <option value="" <?= old_select('category_id', '', $row['category_id'] ?? '') ?
